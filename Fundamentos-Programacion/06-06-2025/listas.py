f4c20214cd489f52a1b1c5ba5559a711ff808b11
Gustavomendezcrts <gu.mendezc@duocuc.cl>
2025-06-06 12:13:54 -0400

    clase

---
+++ Fundamentos-Programacion/06-06-2025/listas.py
@@ -0,0 +1,13 @@
+milista = [1,'hola', 2, 3, 1.5, True]
+
+ 
+
+for i in milista:
+    if i == "holaa":
+        print("Encontré el elemento")
+else:
+    print("no existe el elemento")
+        
+for i in milista:
+    if i == 1:
+        print("Encontré el elemento")
